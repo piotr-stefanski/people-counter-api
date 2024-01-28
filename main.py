9cdc8a6c19d9e71b033a1faf68fcda50a35cae21
Piotr Stefański <piotrekcyk@gmail.com>
2024-01-28 12:13:14 +0100

    endpoint with detect people from images on disk

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+import cv2
 
 app = Flask(__name__)
 api = Api(app)
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+class PeopleCounter(Resource):
+    def get(self):
+        img = cv2.imread('images/pks.jpg')
+        boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+
+        return {'count': len(boxes)}
 
 
 class HelloWorld(Resource):
@@ -10,6 +21,7 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 
+api.add_resource(PeopleCounter, '/')
 api.add_resource(HelloWorld, '/test')
 
 if __name__ == '__main__':
